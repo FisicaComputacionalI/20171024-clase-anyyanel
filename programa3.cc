6fed935239bcba3b63c34e4c422937408a7a00f9
anyyanel <31254545+anyyanel@users.noreply.github.com>
2017-10-24 13:33:51 -0500

    Add files via upload
    Programa que indica si un número es primo

---
+++ programa3.cc
@@ -0,0 +1,26 @@
+//Saber si un numero es primo
+#include<iostream>
+using namespace std;
+
+int main(){
+ long  N=12;
+  long flag=0;
+  long divisor=0;
+  cout<<"inserte un numero_"<<endl;
+  cin>>N;
+  for(long i=2; i<=N/2; i++)
+    {
+      if (N%i ==0){
+	flag=1;
+	divisor=i;
+	break;
+      }
+    }
+  if (flag==1){
+    cout<<"Tu numero no es primo"<<endl;
+  cout<<"porque se divide entre" <<divisor <<endl;
+}
+  else
+    cout<<"tu numero es primo"<<endl;
+  return 0;
+}
